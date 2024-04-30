a0042a6aaa684e9133077f6f95119a62480359ee
shukawam <shukawam@gmail.com>
2024-04-30 15:10:45 +0900

    Fixed minor bug of spaghetti

--- service/chef/internal/recipe/spaghetti.go
+++ service/chef/internal/recipe/spaghetti.go
@@ -18,29 +18,49 @@ func Spaghetti(c *gin.Context) {
 		ctx, span := tracer.Start(ctx, "食材の準備")
 		defer span.End()
 		logger := logging.WithTrace(ctx, logger)
-		useMaterials(logger, "ブルーベリー", "", "適量", "")
-		useMaterials(logger, "牛乳", "", "適量", "")
-		useMaterials(logger, "ヨーグルト", "", "適量", "")
+		useMaterials(logger, "にんにく", "5", "g", "国産がいいぞ")
+		useMaterials(logger, "オリーブオイル", "15", "ml", "")
+		useMaterials(logger, "鷹の爪", "2", "g", "")
+		useMaterials(logger, "パセリ", "5", "g", "普通のパセリでもイタリアンパセリでもOK! 乾燥パセリ使うくらいなら使わない方がマシ")
+		useMaterials(logger, "パスタ", "100", "g", "")
+		useMaterials(logger, "水", "2", "l", "")
+		useMaterials(logger, "塩", "20", "g", "1%くらいにすると美味しく茹でれる")
 
 		time.Sleep(time.Duration(100) * time.Millisecond)
 		transitionTo(logger, "食材の準備OK", "")
 	}()
 
 	func() {
-		ctx, span := tracer.Start(ctx, "ミキサーで混ぜる")
+		ctx, span := tracer.Start(ctx, "ソースを作る")
 		defer span.End()
 		logger := logging.WithTrace(ctx, logger)
-		doCookAction(logger, "", "混ぜる", "何も考えるな！")
+		doCookAction(logger, "にんにく", "炒める", "超弱火")
+		transitionTo(logger, "にんにくの香りをオイルに移す", "")
+		doCookAction(logger, "鷹の爪", "炒める", "2秒くらいでいいぞ")
+		transitionTo(logger, "鷹の爪の香りをオイルに移す", "")
+		doCookAction(logger, "パセリ", "半量いれる", "")
+		transitionTo(logger, "パセリが入れ終わる", "")
+		doCookAction(logger, "茹で汁", "茹で汁を入れて乳化する", "オイルと同量くらいがおすすめ")
 
 		time.Sleep(time.Duration(480) * time.Millisecond)
-		transitionTo(logger, "混ぜおわる", "")
+		transitionTo(logger, "ソースが作り終わる", "")
 	}()
 
 	func() {
-		ctx, span := tracer.Start(ctx, "仕上げ")
+		ctx, span := tracer.Start(ctx, "パスタを茹でる")
 		defer span.End()
 		logger := logging.WithTrace(ctx, logger)
-		doCookAction(logger, "", "コップに移す", "")
+		doCookAction(logger, "パスタ", "茹でる", "何も考えるな！")
+
+		time.Sleep(time.Duration(480) * time.Millisecond)
+		transitionTo(logger, "茹で終わる", "")
+	}()
+
+	func() {
+		ctx, span := tracer.Start(ctx, "ソースとパスタを和える")
+		defer span.End()
+		logger := logging.WithTrace(ctx, logger)
+		doCookAction(logger, "", "ソースとパスタを和える", "")
 		time.Sleep(time.Duration(30) * time.Millisecond)
 	}()
 
